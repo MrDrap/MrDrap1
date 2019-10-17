bdf54d472f46abb959b94437176a050c5ac0c820
Viacheslav Sazonov <viacheslav.sazonov@seagullinc.co>
2019-10-17 12:12:59 +0300

    Updates

---
+++ precache-manifest.068a10a8cecf90f95f9be7f26fe3d7e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a7f38de748771553cc9a1944762bdba",
+    "url": "/MrDrap1/index.html"
+  },
+  {
+    "revision": "f2045420538188091c48",
+    "url": "/MrDrap1/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "c9e61e844ab8c9b0091d",
+    "url": "/MrDrap1/static/css/main.6bb50df2.chunk.css"
+  },
+  {
+    "revision": "f2045420538188091c48",
+    "url": "/MrDrap1/static/js/2.c5c7d99e.chunk.js"
+  },
+  {
+    "revision": "c9e61e844ab8c9b0091d",
+    "url": "/MrDrap1/static/js/main.a831b116.chunk.js"
+  },
+  {
+    "revision": "8383996872f5532588dc",
+    "url": "/MrDrap1/static/js/runtime-main.d5c608c8.js"
+  },
+  {
+    "revision": "8ac90c27280668ff255e460d68f8615e",
+    "url": "/MrDrap1/static/media/logo.8ac90c27.svg"
+  }
+]);
