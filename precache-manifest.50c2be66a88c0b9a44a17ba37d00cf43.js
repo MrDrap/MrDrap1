a0c798b862b08766a61e433bafe00154f60c233c
Viacheslav Sazonov <viacheslav.sazonov@seagullinc.co>
2019-10-23 13:19:38 +0300

    Updates

---
+++ precache-manifest.50c2be66a88c0b9a44a17ba37d00cf43.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7cf0e3b0e9d23e5c72cdf7c63648e73",
+    "url": "/MrDrap1/index.html"
+  },
+  {
+    "revision": "5eefe0a635b8bfd14c3f",
+    "url": "/MrDrap1/static/css/2.46cdc163.chunk.css"
+  },
+  {
+    "revision": "f557147e69c3c89cc878",
+    "url": "/MrDrap1/static/css/main.b2dcf9c4.chunk.css"
+  },
+  {
+    "revision": "5eefe0a635b8bfd14c3f",
+    "url": "/MrDrap1/static/js/2.013dce10.chunk.js"
+  },
+  {
+    "revision": "f557147e69c3c89cc878",
+    "url": "/MrDrap1/static/js/main.f0caca16.chunk.js"
+  },
+  {
+    "revision": "8383996872f5532588dc",
+    "url": "/MrDrap1/static/js/runtime-main.d5c608c8.js"
+  },
+  {
+    "revision": "8ac90c27280668ff255e460d68f8615e",
+    "url": "/MrDrap1/static/media/logo.8ac90c27.svg"
+  },
+  {
+    "revision": "aa3be005a335ce4ed8b8ac1c4a41836f",
+    "url": "/MrDrap1/static/media/unfold-icon.aa3be005.svg"
+  }
+]);
