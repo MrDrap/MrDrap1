32fabebca70c3b5331ea9de7d70062299d21bc3d
Viacheslav Sazonov <viacheslav.sazonov@seagullinc.co>
2019-10-24 11:50:10 +0300

    .

---
+++ src/components/TransactionsPage/TransactionsTable/TransactionsTable.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Table from '../../Table/Table';
+import { formatDate } from '../../../utils';
+
+
+
+function TransactionsTable({ transactions, onRowClick }) {
+  const columns = [
+    { title: 'Дата', format: date => formatDate(date) },
+    { title: 'Отправитель' },
+    { title: 'Получатель' , styles: { maxWidth: '220px' }},
+    { title: 'Сумма' },
+    { title: 'Статус' }
+    
+  ];
+
+  return (
+    <Table 
+      columns={ columns } 
+      data={ transactions } 
+      onRowClick={ onRowClick }
+      selectable
+    />
+  );
+}
+
+export default TransactionsTable;
